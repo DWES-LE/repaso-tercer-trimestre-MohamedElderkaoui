f97fb5b135e1cd1cf7586b87535b558af92188ef
mohamed <mderkaoui10@gmail.com>
2023-04-15 12:25:27 +0200

    mohame init

---
+++ blog/models.py
@@ -0,0 +1,433 @@
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+from django.http import JsonResponse
+
+from modelcluster.fields import ParentalKey
+
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import  InlinePanel
+from wagtail.search import index
+from django.db import models
+from wagtail.models import Page, PageManager
+
+# New imports added for ClusterTaggableManager, TaggedItemBase, MultiFieldPanel
+
+from wagtail.snippets.models import register_snippet
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField
+from wagtail.search import index
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.search import index
+
+
+# Keep the definition of BlogIndexPage, and add:
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )# ...
+
+from wagtail.models import PreviewableMixin
+
+# ...
+
+@register_snippet
+class Advert(PreviewableMixin, models.Model):
+    url = models.URLField(null=True, blank=True)
+    text = models.CharField(max_length=255)
+
+    panels = [
+        FieldPanel('url'),
+        FieldPanel('text'),
+    ]
+
+    @property
+    def preview_modes(self):
+        return PreviewableMixin.DEFAULT_PREVIEW_MODES + [("alt", "Alternate")]
+
+    def get_preview_template(self, request, mode_name):
+        templates = {
+            "": "demo/previews/advert.html",  # Default preview mode
+            "alt": "demo/previews/advert_alt.html",  # Alternate preview mode
+        }
+        return templates.get(mode_name, templates[""])
+
+    def get_preview_context(self, request, mode_name):
+        context = super().get_preview_context(request, mode_name)
+        if mode_name == "alt":
+            context["extra_context"] = "Alternate preview mode"
+        return context
+from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail import blocks
+from wagtail.admin.panels import FieldPanel
+from wagtail.images.blocks import ImageChooserBlock
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    author = models.CharField(max_length=255)
+    date = models.DateField("Post date")
+    body = StreamField([
+                ('person', blocks.StructBlock([
+                    ('first_name', blocks.CharBlock()),
+                    ('surname', blocks.CharBlock()),
+                    ('photo', ImageChooserBlock(required=False)),
+                    ('biography', blocks.RichTextBlock()),
+                ])),
+                ('heading', blocks.CharBlock(form_classname="title")),
+                ('paragraph', blocks.RichTextBlock()),
+                ('image', ImageChooserBlock()),
+                    ], use_json_field=True)
+    content_panels = Page.content_panels + [
+        FieldPanel('author'),
+        FieldPanel('date'),
+        FieldPanel('body'),
+    ]
+
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    use_other_template = models.BooleanField()
+    
+    ajax_template = 'other_template_fragment.html'
+    template = 'other_template.html'
+
+    def get_template(self, request, *args, **kwargs):
+        if self.use_other_template:
+            return 'blog/other_blog_page.html'
+
+        return 'blog/blog_page.html'
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+    def serve(self, request):
+        return JsonResponse({
+            'title': self.title,
+            'body': self.body,
+            'date': self.date,
+
+            # Resizes the image to 300px width and gets a URL to it
+            'feed_image': self.feed_image.get_rendition('width-300').url,
+        })
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+    
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+
+        # Add extra variables and return the updated context
+        context['blog_entries'] = BlogPage.objects.child_of(self).live()
+        return context
+from django.db import models
+
+# New imports added for ParentalKey, Orderable, InlinePanel
+
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('caption'),
+        
+    ]
+    content_panels = [
+    
+
+    InlinePanel('related_links', label="Related links"),
+]
+
+class BlogTagIndexPage(Page):
+
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['blogpages'] = blogpages
+        return context
+
+from wagtail.snippets.models import register_snippet
+
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    icon = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('icon'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'blog categories'
+class LandingPage(Page):
+
+    page_description = "Use this page for converting users"
+    
+from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.models import Orderable
+
+
+class BlogPageRelatedLink(Orderable):
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='related_links')
+    name = models.CharField(max_length=255)
+    url = models.URLField()
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('url'),
+    ]
+class NewsItemPage(Page):
+    publication_date = models.DateField()
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    class Meta:
+        ordering = ('-publication_date', )  # will not work
+
+class EventPageManager(PageManager):
+    """ Custom manager for Event pages """
+
+class EventPage(Page):
+    start_date = models.DateField()
+
+    objects = EventPageManager()
+from django.utils import timezone
+from wagtail.models import Page, PageManager, PageQuerySet
+
+class EventPageQuerySet(PageQuerySet):
+    def future(self):
+        today = timezone.localtime(timezone.now()).date()
+        return self.filter(start_date__gte=today)
+
+EventPageManager = PageManager.from_queryset(EventPageQuerySet)
+
+class EventPage(Page):
+    start_date = models.DateField()
+
+    objects = EventPageManager()
+    description = models.TextField()
+    date = models.DateField()
+    IS_PRIVATE_CHOICES = (
+        (False, "Public"),
+        (True, "Private"),
+    )
+
+    is_private = models.BooleanField(choices=IS_PRIVATE_CHOICES)
+
+    search_fields = Page.search_fields + [
+        # Index the human-readable string for searching.
+        index.SearchField('get_is_private_display'),
+
+        # Index the boolean value for filtering.
+        index.FilterField('is_private'),
+    ]
+
+    search_fields = Page.search_fields + [ # Inherit search_fields from Page
+        index.SearchField('description'),
+        index.FilterField('date'),
+    ]
+
+from wagtail.search import index
+
+class Book(models.Model, index.Indexed):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    
+    
+    search_fields = [
+        index.SearchField('title'),
+        index.FilterField('published_date'),
+
+        index.RelatedFields('author', [
+            index.SearchField('name'),
+            index.FilterField('date_of_birth'),
+        ]),
+    ]
+
+
+from wagtail.search import index
+
+class Author(models.Model, index.Indexed):
+    name = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    books = models.ManyToManyField('Book', related_name='authors')
+
+    search_fields = [
+        index.SearchField('name'),
+        index.FilterField('date_of_birth'),
+
+        index.RelatedFields('books', [
+            index.SearchField('title'),
+            index.FilterField('published_date'),
+        ]),
+    ]
+    
+class BookPage(Page):
+    # ...
+    def get_related_link_titles(self):
+        # Get list of titles and concatenate them
+        return '\n'.join(self.related_links.all().values_list('name', flat=True))
+
+    search_fields = Page.search_fields + [
+        # ...
+        index.SearchField('get_related_link_titles'),
+    ]
+    from wagtail.search import index
+# import GENRE_CHOICES
+
+# import template
+from django import template
+@register_snippet
+class Advert(models.Model):
+    url = models.URLField(null=True, blank=True)
+    text = models.CharField(max_length=255)
+
+    panels = [
+        FieldPanel('url'),
+        FieldPanel('text'),
+    ]
+
+    def __str__(self):
+        return self.text
+    register = template.Library()
+
+# ...
+from django import template
+# import demo
+
+
+
+register = template.Library()
+# Advert snippets
+@register.inclusion_tag('demo/tags/adverts.html', takes_context=True)
+def adverts(context):
+    return {
+        'adverts': Advert.objects.all(),
+        'request': context['request'],
+    }
+from django import template
+
+register = template.Library()
+
+# ...
+
+# Advert snippets
+@register.inclusion_tag('demo/tags/adverts.html', takes_context=True)
+def adverts(context):
+    return {
+        'adverts': Advert.objects.all(),
+        'request': context['request'],
+    }
+from django import template
+
+register = template.Library()
+
+# ...
+
+# Advert snippets
+@register.inclusion_tag('demo/tags/adverts.html', takes_context=True)
+def adverts(context):
+    return {
+        'adverts': Advert.objects.all(),
+        'request': context['request'],
+    }
+    
+    from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+from taggit.models import TaggedItemBase
+from taggit.managers import TaggableManager
+
+class AdvertTag(TaggedItemBase):
+    # content_object = ParentalKey('demo.Advert', on_delete=models.CASCADE, related_name='tagged_items')
+    pass
+
+@register_snippet
+class Advert(ClusterableModel):
+    # ...
+    tags = TaggableManager(through=AdvertTag, blank=True)
+
+    panels = [
+        # ...
+        FieldPanel('tags'),
+    ]
+# models.py
+from django.db import models
+from wagtail.admin.filters import WagtailFilterSet
+
+
+class Member(models.Model):
+    class ShirtSize(models.TextChoices):
+        SMALL = "S", "Small"
+        MEDIUM = "M", "Medium"
+        LARGE = "L", "Large"
+        EXTRA_LARGE = "XL", "Extra Large"
+
+    name = models.CharField(max_length=255)
+    shirt_size = models.CharField(max_length=5, choices=ShirtSize.choices, default=ShirtSize.MEDIUM)
+
+    def get_shirt_size_display(self):
+        return self.ShirtSize(self.shirt_size).label
+
+    get_shirt_size_display.admin_order_field = "shirt_size"
+    get_shirt_size_display.short_description = "Size description"
+
+
+class MemberFilterSet(WagtailFilterSet):
+    class Meta:
+        model = Member
+        fields = ["shirt_size"]
