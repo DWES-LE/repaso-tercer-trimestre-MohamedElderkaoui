1bf433ea66f76f9b1edfc00e38b881f0da073959
mohamed <mderkaoui10@gmail.com>
2023-04-17 11:14:41 +0200

    ya

--- home/models.py
+++ home/models.py
@@ -7,7 +7,11 @@ from wagtail.admin.panels import FieldPanel
 
 class HomePage(Page):
     body = RichTextField(blank=True)
+    video = models.FileField( blank=True)
+    image = models.ImageField( blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('body'),
-    ]
+        FieldPanel('video'),
+        FieldPanel('image'),
+        ]
