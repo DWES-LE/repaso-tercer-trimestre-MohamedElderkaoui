d5adb5eaf5a4db9c92a570954455b65e392291e2
mohamed <mderkaoui10@gmail.com>
2023-05-10 18:53:37 +0200

    fecha: 2023-05-10 hora: 18:53:36,93 por: MOHAMED que a traido:

--- subir.bat
+++ subir.bat
@@ -1,4 +1,8 @@
-
+python manage.py dumpdata --natural-foreign --indent 2 ^
+    -e contenttypes -e auth.permission ^
+    -e wagtailcore.groupcollectionpermission ^
+    -e wagtailcore.grouppagepermission -e wagtailimages.rendition ^
+    -e sessions > data.json
 git add .
 git commit -m "fecha: %date% hora: %time% por: %username% que a traido: %1"
 git push
