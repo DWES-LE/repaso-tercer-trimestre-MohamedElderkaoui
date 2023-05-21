d2624dbec9f3671a7213556a4d6af989846ae9c6
mohamed <mderkaoui10@gmail.com>
2023-05-21 18:00:30 +0200

    dia 2023-05-21 hora 18:00:29,95 por MOHAMED

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -93,3 +93,25 @@ class BlogTagIndexPage(Page):
         context = super().get_context(request)
         context['blogpages'] = blogpages
         return context
+
+from wagtail.snippets.models import register_snippet
+
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    icon = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('icon'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'blog categories'
