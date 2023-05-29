6f7e06d8fa7da02fcd4d9ff9b66b5af1e76da68e
mohamed <mderkaoui10@gmail.com>
2023-05-29 19:56:55 +0200

    fecha 2023-05-29 hora 19:56:55,59 sitio C:\2DAW\Desarrollo Web en entorno Servidor DAW Diurno\repaso-tercer-trimestre-MohamedElderkaoui ciudad

--- SUBIR.bat
+++ SUBIR.bat
@@ -1,13 +1,14 @@
-copy NUL equipo_page.html
-copy NUL equipo_index_page.html
-copy NUL jugador_page.html
-copy NUL jugador_index_page.html
-copy NUL entrenador_page.html
-copy NUL entrenador_index_page.html
-copy NUL liga_partidos_page.html
-copy NUL liga_clasificacion_page.html
-copy NUL liga_index_page.html
-copy NUL partido_page.html
+echo. > equipo_page.html
+echo. > equipo_index_page.html
+echo. > jugador_page.html
+echo. > jugador_index_page.html
+echo. > entrenador_page.html
+echo. > entrenador_index_page.html
+echo. > liga_partidos_page.html
+echo. > liga_clasificacion_page.html
+echo. > liga_index_page.html
+echo. > partido_page.html
+
 git add .
 git commit -m "fecha %date% hora %time% sitio %cd% ciudad %location%"
 git push 
