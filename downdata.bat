3400bc55039a3e29752542ac1713dc3c52aa6a6a
mohamed <mderkaoui10@gmail.com>
2023-06-13 06:00:00 +0200

    fecha 2023-06-13 hora  6:00:00,45 sitio C:\2DAW\Desarrollo Web en entorno Servidor DAW Diurno\repaso-tercer-trimestre-MohamedElderkaoui ciudad

--- downdata.bat
+++ downdata.bat
@@ -1,8 +1,2 @@
-py manage.py shell < bp.py
 py ./manage.py dumpdata --natural-foreign --indent 2 -e auth.permission -e contenttypes -e wagtailcore.GroupCollectionPermission -e wagtailcore.revision -e wagtailimages.rendition -e sessions -e wagtailsearch.indexentry -e wagtailsearch.sqliteftsindexentry -e wagtailcore.referenceindex -e wagtailcore.pagesubscription -e wagtailcore.modellogentry -e wagtailcore.pagelogentry > bakerydemo.json
-prettier --write bakerydemo.json
-
-
-
-
-python manage.py dumpdata --natural-foreign --indent 2  -e contenttypes -e auth.permission -e wagtailcore.groupcollectionpermission  -e wagtailcore.grouppagepermission -e wagtailimages.rendition  -e wagtailsearch.indexentry -e wagtailsearch.sqliteftsindexentry  -e sessions > data.json
+--write bakerydemo.json
