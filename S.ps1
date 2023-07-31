4b32c20290c8ff8f1afd965b598b7660e18194c2
mohamed <mderkaoui10@gmail.com>
2023-07-31 11:56:01 +0200

    fecha 2023-07-31 hora 11:56:01,10 sitio C:\2DAW\Desarrollo Web en entorno Servidor DAW Diurno\repaso-tercer-trimestre-MohamedElderkaoui ciudad

--- S.ps1
+++ S.ps1
@@ -15,20 +15,3 @@ while ($true) {
     Write-Output "Esperando 10 minutos..."
     Start-Sleep -Seconds 600
 }
-while ($true) {
-    $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    $location = Get-Location
-    $city = "Tu Ciudad"  # Reemplaza "Tu Ciudad" con el nombre de tu ciudad
-
-    Write-Output "Ejecutando git add ."
-    git add .
-
-    Write-Output "Ejecutando git commit"
-    git commit -m "fecha $date sitio $location ciudad $city"
-
-    Write-Output "Ejecutando git push"
-    git push
-
-    Write-Output "Esperando 10 minutos..."
-    Start-Sleep -Seconds 600
-}
